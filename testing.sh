02faba2e960c150f8bb2ed7d1c5c8783820dbf2e
Yusuf Erdem Nacar <yusuferdemnacar@gmail.com>
2021-04-23 20:23:08 +0300

    fixed some testacses

--- testing.sh
+++ testing.sh
@@ -21,3 +21,43 @@ time ./a.out ./deliverables/inputs/testcase15.my ./myoutputs/output15.ll
 time ./a.out ./deliverables/inputs/testcase16.my ./myoutputs/output16.ll
 time ./a.out ./deliverables/inputs/testcase17.my ./myoutputs/output17.ll
 time ./a.out ./deliverables/inputs/testcase18.my ./myoutputs/output18.ll
+
+echo "output 0:"
+lli ./myoutputs/output0.ll
+echo "output 1:"
+lli ./myoutputs/output1.ll
+echo "output 2:"
+lli ./myoutputs/output2.ll
+echo "output 3:"
+lli ./myoutputs/output3.ll
+echo "output 4:"
+lli ./myoutputs/output4.ll
+echo "output 5:"
+lli ./myoutputs/output5.ll
+echo "output 6:"
+lli ./myoutputs/output6.ll
+echo "output 7:"
+lli ./myoutputs/output7.ll
+echo "output 8:"
+lli ./myoutputs/output8.ll
+echo "output 9:"
+lli ./myoutputs/output9.ll
+echo "output 10:"
+lli ./myoutputs/output10.ll
+echo "output 11:"
+lli ./myoutputs/output11.ll
+echo "output 12:"
+lli ./myoutputs/output12.ll
+echo "output 13:"
+lli ./myoutputs/output13.ll
+echo "output 14:"
+lli ./myoutputs/output14.ll
+echo "output 15:"
+lli ./myoutputs/output15.ll
+echo "output 16:"
+lli ./myoutputs/output16.ll
+echo "output 17:"
+lli ./myoutputs/output17.ll
+echo "output 18:"
+lli ./myoutputs/output18.ll
+
