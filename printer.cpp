500ff0f0a8b014f168ba7436416dd0719d91f900
Yusuf Erdem Nacar <yusuferdemnacar@gmail.com>
2021-04-28 19:28:59 +0300

    -Added the functionality that generates LLVM code that prints syntax error message to the console.

--- printer.cpp
+++ printer.cpp
@@ -2,20 +2,20 @@
 
 using namespace std;
 
-Printer::Printer(string filename)
-{
+Printer::Printer(string filename){
+
     this->filename = filename;
     this->output.open(filename);
     this->output << "; ModuleID = 'mylang2ir'\ndeclare i32 @printf(i8*, ...)\n@print.str = constant [4 x i8] c\"%d\\0A\\00\"";
     this->output << "\n\ndefine i32 @main() {\n\nbr label %alloc \n\ncontinue:\n\n";
 }
 
-void Printer::print(string s)
-{
+void Printer::print(string s){
     this->output << s << endl;
 }
-void Printer::finalize(unordered_map<string, string> &pointers)
-{   print("\nret i32 0\n\nalloc:\n");
+
+void Printer::finalize(unordered_map<string, string> &pointers){
+    print("\nret i32 0\n\nalloc:\n");
     for (auto pointer : pointers)
     {
         string second = pointer.second;
@@ -24,14 +24,21 @@ void Printer::finalize(unordered_map<string, string> &pointers)
     }
     print("br label %continue \n\n}");
 }
-void Printer::syntaxError(int line)
-{
+
+void Printer::syntaxError(int line){
+
     this->output.close();
     this->output.open(filename);
-    this->output << "Line " + to_string(line) + ":syntax error";
+    print("; ModuleID = 'mylang2ir'\ndeclare i32 @printf(i8*, ...)\n@print.str = constant [23 x i8] c\"Line %d: syntax error\\0A\\00\"");
+    print("\ndefine i32 @main() {\n");
+
+    print("call i32 (i8*, ...)* @printf(i8* getelementptr ([23x i8]* @print.str, i32 0, i32 0), i32 " + to_string(line) + ")");
+
+    print("\nret i32 0\n\n}");
 }
-Printer::~Printer()
-{
+
+Printer::~Printer(){
+
     if (this->output.is_open())
         this->output.close();
 }
