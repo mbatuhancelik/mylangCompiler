5bf4d39746c736a176b78d553d262c93c40f29bf
batuhan celik <batuhancelik.boun@gmail.com>
2021-05-01 21:49:49 +0300

    testing outputs fixed

--- testing.sh
+++ testing.sh
@@ -14,14 +14,17 @@ mkdir llvmoutputs
 
 for((counter=0; counter<$n; counter++))
 do
-echo " executing ll codes $counter"
+echo "\n executing ll codes $counter\n"
+rm ./llvmoutputs/output$counter
 touch ./llvmoutputs/output$counter
 lli ./deliverables/inputs/testcase$counter.ll >> ./llvmoutputs/output$counter
 done
 
 for((counter=0; counter<$n; counter++))
 do
+echo " "
 echo " checking $counter"
+echo " "
 diff ./deliverables/outputs/testcase$counter.txt ./llvmoutputs/output$counter
 done
 
